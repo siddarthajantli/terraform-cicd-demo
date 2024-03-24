c3a648628b4b60b5f02be35b1e9ad46bad152b44
Siddartha Jantli <39168165+siddarthajantli@users.noreply.github.com>
2024-03-24 22:53:27 +0530

    new

--- ec2.tf
+++ ec2.tf
@@ -97,7 +97,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2key"
+  key_name               = "lab-demo-key"
   user_data              = file("install_techmax.sh")
 
   tags = {
